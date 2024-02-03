c2cdb4e2479075ba5c61a0309439572a6c60de00
unknown <hriskikesh.yadav332@gmail.com>
2024-02-03 18:12:33 +0530

    Chat Section

--- app.py
+++ app.py
@@ -6,14 +6,51 @@ import matplotlib.pyplot as plt
 from streamlit_option_menu import option_menu
 import plotly.graph_objects as go
 import requests
+import os
 from goose3 import Goose
 from wordcloud import WordCloud, STOPWORDS
 import json
+import io
 from PIL import Image
 import base64
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import logging
 
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from PIL import Image
+import plotly.graph_objects as go
+import plotly.io as pio
+
+
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TwitterTweetLoader
+
+from llama_index import SimpleDirectoryReader
+from llama_index import GPTVectorStoreIndex
+from langchain_community.document_loaders import TwitterTweetLoader
+import llama_index
+
+
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+headers = {"Authorization": "Bearer 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"}
+
+def query_summ(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+
+print(os.getenv('TWITTER_API_KEY'))
+
+# TwitterTweetReader = download_loader("TwitterTweetReader")
 
 page_element="""
 <style>
@@ -32,59 +69,118 @@ background-size: cover;
 
 """
 
+
+selected = option_menu(
+    menu_title=None,
+    options=["Main","Chat"],
+    menu_icon=['house', 'chat-dots'],
+    orientation="horizontal")
+
+
 st.markdown(page_element, unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; color: white';>Samachar AI 💬</h1>", unsafe_allow_html=True)
 st.markdown("---")
 
-link=st.text_input("Put the Article link below to check -")
-check=st.button("Check and Analyze")
-if check:
+if selected=="Main":
+    link=st.text_input("Put the Article link below to check -")
+    check=st.button("Check and Analyze")
+    if check:
 
-    def sent(link):
-        senti=[]
-        url = link
-        goose = Goose()
-        articles = goose.extract(url)
-        sentence1 = articles.cleaned_text
-        sid_obj = SentimentIntensityAnalyzer()
-        sentiment_dict = sid_obj.polarity_scores([sentence1])
-        print(sentiment_dict['neg']*100, "% Negative")
-        print(sentiment_dict['pos']*100, "% Positive")
-        print("Review Overall Analysis", end = " ") 
-        if sentiment_dict['compound'] >= 0.05 :
-            senti.append("Positive")
-        elif sentiment_dict['compound'] <= -0.05 :
-            senti.append("Negative")
-        else :
-            senti.append("Neutral")
-        
-        labels = list(sentiment_dict.keys())
-        values = list(sentiment_dict.values())
-        fig = go.Figure()
-        fig.add_trace(go.Scatterpolar(
-            r=values,
-            theta=labels,
-            fill='toself',
-            line=dict(color='darkblue'),
-            marker=dict(size=10, color='royalblue', symbol='circle-open')
-        ))
+        def sent(link):
+            senti=[]
+            url = link
+            goose = Goose()
+            articles = goose.extract(url)
+            sentence1 = articles.cleaned_text
+            sid_obj = SentimentIntensityAnalyzer()
+            sentiment_dict = sid_obj.polarity_scores([sentence1])
+            print(sentiment_dict['neg']*100, "% Negative")
+            print(sentiment_dict['pos']*100, "% Positive")
+            print("Review Overall Analysis", end = " ") 
+            if sentiment_dict['compound'] >= 0.05 :
+                senti.append("Positive")
+            elif sentiment_dict['compound'] <= -0.05 :
+                senti.append("Negative")
+            else :
+                senti.append("Neutral")
+            
+            labels = list(sentiment_dict.keys())
+            values = list(sentiment_dict.values())
+            fig = go.Figure()
+            fig.add_trace(go.Scatterpolar(
+                r=values,
+                theta=labels,
+                fill='toself',
+                line=dict(color='darkblue'),
+                marker=dict(size=10, color='royalblue', symbol='circle-open')
+            ))
 
-        fig.update_layout(
-            polar=dict(
-                radialaxis=dict(
-                    visible=True,
-                    range=[0, 1]
-                )),
-            showlegend=False,
-            title={
-                'text': "Sentiment Analysis",
-                'y':0.9,
-                'x':0.5,
-                'xanchor': 'center',
-                'yanchor': 'top'},
-            font=dict(family="Courier New, monospace", size=12, color="#7f7f7f")
+            fig.update_layout(
+                polar=dict(
+                    radialaxis=dict(
+                        visible=True,
+                        range=[0, 1]
+                    )),
+                showlegend=False,
+                title={
+                    'text': "Sentiment Analysis",
+                    'y':0.9,
+                    'x':0.5,
+                    'xanchor': 'center',
+                    'yanchor': 'top'},
+                font=dict(family="Courier New, monospace", size=12, color="#7f7f7f")
+            )
+
+            return fig
+
+        f1=sent(link)
+        st.plotly_chart(f1)
+
+
+if selected=="Chat":
+    load_dotenv()
+    print(os.getenv('OPENAI_API_KEY'))
+    os.getenv('OPENAI_API_KEY')
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Welcome to Llama Press, How may I help you?"):
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    def chat(p):
+        query = p
+        # reader = TwitterTweetReader(os.getenv('BEARER_TOKEN'))
+        # documents = reader.load_data(["ANI"])
+        # documents1 = reader.load_data(["ZeeNews"])
+        # documents2 = reader.load_data(["TV9Bharatvarsh"])
+        # documents3 = reader.load_data(["Republic_Bharat"])
+        # documents4 = reader.load_data(["AajTak"])
+        loader = TwitterTweetLoader.from_secrets(
+       
+            access_token=os.getenv('ACCESS_TOKEN'),
+            access_token_secret=os.getenv('ACCESS_TOKEN_SECRET'),
+            consumer_key=os.getenv('CONSUMER_API_KEY'),
+            consumer_secret=os.getenv('CONSUMER_API_KEY_SECRET'),
+            twitter_users=['elonmusk'],
+            number_tweets=50,
         )
+        documents = loader.load()
+        st.write(documents[:5])
+        
+        # agent = llama_index.GPTVectorStoreIndex.from_documents(documents)
+        # chat_engine = agent.as_chat_engine(verbose=True)
+        # response = chat_engine.chat(query)
+        # return response
 
-        st.plotly_chart(fig)
-
-    sent(link)
+    result = chat(prompt)
+    response = f"Echo: {result}"
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
